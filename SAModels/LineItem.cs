2af55a4ffe69afe8e1633d942fd5bd507f3d6cb7
ieolivares <ieolivares9742@gmail.com>
2021-07-09 18:58:57 -0500

    Updated model

--- SAModels/LineItem.cs
+++ SAModels/LineItem.cs
@@ -2,7 +2,7 @@ namespace SAModels
 {
     public class LineItem
     {
-        public string Product { get; set; }
+        public Product Item { get; set; }
         public int Quantity { get; set; }
     }
 }
